e9ba63bb56a3daaa86d64ca395a2945fe2a4a433
Zicka67 <art.book@hotmail.fr>
2023-03-22 15:19:00 +0100

    Modif on form casting for add a foreach

--- view/InsertAdmin/insertCasting.php
+++ view/InsertAdmin/insertCasting.php
@@ -3,19 +3,34 @@ ob_start();
 ?>
 <form action="index.php?action=insertCastings" method="post">
 
+
     <div class="form">
-        <label for="film_id">film_id</label>
-        <input type="number" name="film_id" id="film_id">
+        <label for="film_id">Name </label>
+        <select name="film_id" id="film_id">
+            <?php
+            foreach ($requestFilm->fetchAll() as $film) {
+                echo "<option value='" . $film['film_id'] . "'>" . $film['film_name'] . "</option>";
+            }; ?>
+        </select>
     </div>
 
     <div class="form">
-        <label for="actor_id">actor_id</label>
-        <input type="number" name="actor_id" id="actor_id">
+        <label for="person_id">Person </label>
+        <select name="person_id" id="person_id">
+            <?php
+            foreach ($requestPerson->fetchAll() as $person) {
+                echo "<option value='" . $person['id_person'] . "'>" . $person['lname'] . "</option>";
+            } ?>
+        </select>
     </div>
 
     <div class="form">
-        <label for="role_id">role_id</label>
-        <input type="number" name="role_id" id="role_id">
+        <label for="role_id">Role </label>
+        <select name="role_id" id="role_id">
+            <?php
+            foreach ($requestRole->fetchAll() as $role) {
+                echo "<option value='" . $role['id_role'] . "'>" . $role['role_name'] . "</option>";
+            }; ?>
     </div>
 
 
