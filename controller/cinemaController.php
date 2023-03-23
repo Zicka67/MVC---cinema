7f1d170fbb460cd1a9b8ca7bb6cff0b18143ded8
Zicka67 <art.book@hotmail.fr>
2023-03-23 13:06:51 +0100

    Modif on detailsCategory function

--- controller/cinemaController.php
+++ controller/cinemaController.php
@@ -20,25 +20,25 @@ class CinemaController
             WHERE film.id_film = 1
             "
         );
-
+        
         include 'view/homePage.php';
     }
-
+    
     //Admin
     public function admin()
-	{
-		require "view/admin.php";
-	}
-
+    {
+        require "view/admin.php";
+    }
+    
     //Liste des films
     public function listFilms()
     {
         // La méthode listFilms du contrôleur CinemaController utilise la méthode connectToDb de la classe Connect 
         // pour établir une connexion à la DB.
         $pdo = Connect::connectToDb();
-
-
-
+        
+        
+        
         // Le résultat est stocké dans la variable $pdo pour être utilisé dans la requête SQL.
         $request = $pdo->query(
             "
@@ -51,9 +51,9 @@ class CinemaController
         //Link à la view correspondante
         require "view/listFilms.php";
     }
-
+    
     // *****************************************************
-
+    
     //Liste des directors
     public function listDirectors()
     {
@@ -65,16 +65,16 @@ class CinemaController
             INNER JOIN person ON director.person_id = person.id_person
             "
         );
-
+        
         require "view/listDirectors.php";
     }
-
+    
     // *****************************************************
-
+    
     //Liste des acteurs
     public function listActors()
     {
-
+        
         $pdo = Connect::connectToDb();
         $request = $pdo->query(
             "
@@ -84,16 +84,16 @@ class CinemaController
             
             "
         );
-
+        
         require "view/listActors.php";
     }
-
+    
     // *****************************************************
-
+    
     //Liste des catégories
     public function listCategorys()
     {
-
+        
         $pdo = Connect::connectToDb();
         $request = $pdo->query(
             "
@@ -101,12 +101,12 @@ class CinemaController
             FROM category 
             "
         );
-
+        
         require "view/listCategorys.php";
     }
-
+    
     // *****************************************************
-
+    
     //Liste des roles
     public function listRoles()
     {
@@ -117,12 +117,12 @@ class CinemaController
             FROM role
             "
         );
-
+        
         require "view/listRoles.php";
     }
-
+    
     // *****************************************************
-
+    
     //Liste des castings
     public function listCastings()
     {
@@ -137,18 +137,18 @@ class CinemaController
             INNER JOIN actor ON person.id_person = actor.person_id
             "
         );
-
+        
         require "view/listCastings.php";
     }
-
-
+    
+    
     // ************************************************** PARTICULAR REQUEST **************************************************
-
-
+    
+    
     //Un film en particulier
     public function detailsFilm($id_film)
     {
-
+        
         $pdo = Connect::connectToDb();
         // $film_name = a l'id_film qui est en argument de la fonction detailsfilm au dessus
         $film_name = "$id_film";
@@ -165,37 +165,37 @@ class CinemaController
         );
         //Execution
         $requestFilmInfos->execute(["id_film" => $id_film]); //"id_film" va être ici = a 1 (film.id_film = 1 dans la requete SQL) et $id_film = a l'argument de la fonction detailsFilm
-
+        
         //PREPARATION INFO ACTOR
         $requestCastingInfos = $pdo->prepare(
             "
             SELECT fname, lname, role_name, id_actor
-			FROM person
-			INNER JOIN actor ON person.id_person = actor.person_id
-			INNER JOIN casting ON casting.actor_id = actor.id_actor
-			INNER JOIN role ON role.id_role = casting.role_id
-			WHERE casting.film_id = :id_film
+            FROM person
+            INNER JOIN actor ON person.id_person = actor.person_id
+            INNER JOIN casting ON casting.actor_id = actor.id_actor
+            INNER JOIN role ON role.id_role = casting.role_id
+            WHERE casting.film_id = :id_film
             "
         );
         //Execution
         $requestCastingInfos->execute(["id_film" => $id_film]);
-
+        
         //PREPARATION INFO director
         $requestCastingInfosRea = $pdo->prepare(
             "
             SELECT film_name, fname, lname, note, id_director
-			FROM film
-			INNER JOIN director ON film.director_id = director.id_director
-			INNER JOIN person ON director.person_id = person.id_person
-			WHERE  film.id_film = :id_film
+            FROM film
+            INNER JOIN director ON film.director_id = director.id_director
+            INNER JOIN person ON director.person_id = person.id_person
+            WHERE  film.id_film = :id_film
             "
         );
         //Execution
         $requestCastingInfosRea->execute(["id_film" => $id_film]);
-
+        
         require "view/detailsFilm.php";
     }
-
+    
     //les acteurs du film en particulier
     public function actorFromFilm($id_film)
     {
@@ -213,21 +213,21 @@ class CinemaController
             WHERE film_id = :id_film
             "
         );
-
+        
         $requestActorFromFilm->execute(["id_film" => $id_film]);
-
-
+        
+        
         require "view/detailsFilm.php";
     }
-
+    
     //Un director en particulier
     public function detailsDirector($id_director)
     {
-
+        
         $pdo = Connect::connectToDb();
-
+        
         $director_name = "$id_director";
-
+        
         $requestDirectorInfos = $pdo->prepare(
             "
             SELECT fname, lname, sexe, DATE_FORMAT(bdate, '%d-%m-%Y') AS bdate, url_img
@@ -236,10 +236,10 @@ class CinemaController
             WHERE director.id_director = :id_director
             "
         );
-
+        
         $requestDirectorInfos->execute(["id_director" => $id_director]);
-
-
+        
+        
         $requestDirectorFilmo = $pdo->prepare(
             "
             SELECT id_director, id_film, film_name, dt_release
@@ -255,19 +255,19 @@ class CinemaController
             // WHERE director.id_director = :id_director
             // "
         );
-
+        
         $requestDirectorFilmo->execute(["id_director" => $id_director]);
-
-
+        
+        
         require "view/detailsDirector.php";
     }
-
+    
     //Un acteur en particulier
     public function detailsActor($id_actor)
     {
-
+        
         $pdo = Connect::connectToDb();
-
+        
         $requestActorInfos = $pdo->prepare(
             "
             SELECT id_actor, fname, lname, sexe, DATE_FORMAT(bdate, '%d/%m/%Y') AS bdate, url_img
@@ -277,9 +277,9 @@ class CinemaController
             "
         );
         $requestActorInfos->execute(["id_actor" => $id_actor]);
-
-
-
+        
+        
+        
         $requestActorFilmo = $pdo->prepare(
             "
             SELECT id_actor, id_film, film_name, dt_release, role_name
@@ -290,37 +290,50 @@ class CinemaController
             WHERE id_actor = :id_actor
             "
         );
-
+        
         $requestActorFilmo->execute(["id_actor" => $id_actor]);
-
-
+        
+        
         require "view/detailsActor.php";
     }
-
-
-    //Une category en particulier 
+    
+    
     public function detailsCategory($id_category)
     {
-
-
+        
+        // Infos category
         $pdo = Connect::connectToDb();
-        $category_name = "$id_category";
-        $requestCategoryInfos = $pdo->prepare(
+
+        $SqlCategoryInfo = $pdo->prepare("
+        SELECT category_name, id_category
+        FROM category
+        WHERE id_category = :id_category
+        ");
+        $SqlCategoryInfo->execute(["id_category" => $id_category]);
+        
+        
+        // Liste film par category
+        $pdo = Connect::connectToDb();
+
+        $SqlCategoryFilmo = $pdo->prepare(
             "
-            SELECT id_category, category_name
-            FROM category
-            WHERE id_category = :id_category
+            SELECT film_name, dt_release, film_length
+            FROM category_film
+            INNER JOIN film ON category_film.id_film = film.id_film
+            WHERE category_film.id_category = :id_category
             "
         );
-        $requestCategoryInfos->execute(["id_category" => $id_category]);
-
+        $SqlCategoryFilmo->execute(["id_category" => $id_category]);
+        
+        
         require "view/detailsCategory.php";
     }
-
+    
+    
     public function detailsRole($id_role)
     {
-
-
+        
+        
         $pdo = Connect::connectToDb();
         $category_name = "$id_role";
         $requestRolesInfos = $pdo->prepare(
@@ -332,29 +345,29 @@ class CinemaController
             "
         );
         $requestRolesInfos->execute(["id_role" => $id_role]);
-
+        
         require "view/detailsActors.php";
     }
-
+    
     //Category filmo                                                              //A CORRIGER
-    public function requestCategory($id_category)
-    {
-
-        $pdo = Connect::connectToDb();
-        $category_name = "$id_category";
-        $requestCategoryFilmo = $pdo->prepare(
-            "
-            SELECT film_name, dt_release, film_length, category_id, category_name, id_film
-            FROM film 
-            INNER JOIN category ON film.id_category = category.category_id
-            WHERE id_category = :id_category
-            "
-        );
-        $requestCategoryFilmo->execute(["id_category" => $id_category]);
-
-
-        require "view/detailsCategory.php";
-    }
+    // public function requestCategory($id_category)
+    // {
+        
+    //     $pdo = Connect::connectToDb();
+    //     $category_name = "$id_category";
+    //     $requestCategoryFilmo = $pdo->prepare(
+    //         "
+    //         SELECT film_name, dt_release, film_length, category_id, category_name, id_film
+    //         FROM film 
+    //         INNER JOIN category ON film.id_category = category.category_id
+    //         WHERE id_category = :id_category
+    //         "
+    //     );
+    //     $requestCategoryFilmo->execute(["id_category" => $id_category]);
+        
+        
+    //     require "view/detailsCategory.php";
+    // }
     
 }
 
